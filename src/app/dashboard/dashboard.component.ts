627a0545e409ecb70c2c781694ca4c832dee424d
mohammadafnan29 <mohammadafnan29@gmail.com>
2021-11-20 21:12:18 +0500

    added filter in dashboardd

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class DashboardComponent implements OnInit {
-   search : any;
+  search: any;
 
   // Placecard: string[] = ['USA', 'Maldive', 'Turkey', 'Baku'];
   Placecard: any[] = [{
